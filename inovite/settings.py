d7542381251ea9106c84a42f365a44f98052a8f1
Raj Patil <rxjpatil@gmail.com>
2025-09-18 00:34:50 +0530

    settings updated

--- inovite/settings.py
+++ inovite/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_URL='/media/'
